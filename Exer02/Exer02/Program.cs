55cbb02b0ffe4aaae42e6ec55a6a4902bee26da3
project <joaoalvaro700@gmail.com>
2025-01-16 22:18:51 -0300

    Exer02

--- Exer02/Exer02/Program.cs
+++ Exer02/Exer02/Program.cs
@@ -1,48 +1,56 @@
-﻿using System;
-using System.Linq;
-
-namespace Exer02
+﻿namespace Exer02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            double[] valores = {
-                22174.1664, 24537.6698, 26139.6134, 0.0, 0.0, 26742.6612, 0.0,
-                42889.2258, 46251.174, 11191.4722, 0.0, 0.0, 3847.4823, 373.7838,
-                2659.7563, 48924.2448, 18419.2614, 0.0, 0.0, 35240.1826, 43829.1667,
-                18235.6852, 4355.0662, 13327.1025, 0.0, 0.0, 25681.8318, 1718.1221,
-                13220.495, 8414.61
-            };
+            Console.WriteLine("Digite um número.");
+            Console.WriteLine("Digite -1 para encerrar.");
 
-            double menor = double.MaxValue;
-            double maior = double.MinValue;
-            double soma = 0, media;
-            int qtd = 0, diasAcima;
+            int numero;
 
-            foreach (double valor in valores)
+            do
             {
-                if (valor > 0)
+                Console.Write("Digite um número: ");
+                numero = int.Parse(Console.ReadLine());
+
+                if (numero != -1)
                 {
-                    if (valor < menor)
-                        menor = valor;
+                    if (Fibonacci(numero))
+                    {
+                        Console.WriteLine(numero + " pertence à sequência.");
+                    }
+                    else
+                    {
+                        Console.WriteLine(numero + " não pertence à sequência.");
+                    }
+                }
 
-                    if (valor > maior)
-                        maior = valor;
+            } while (numero != -1);
 
-                    soma += valor;
-                    qtd++;
+            Console.WriteLine("Programa encerrado.");
+        }
+
+        static bool Fibonacci(int num)
+        {
+            int a = 0, b = 1, temp;
+            bool valido = false;
+
+            while (b <= num && !valido)
+            {
+                if (b == num)
+                {
+                    valido = true;
+                }
+                else
+                {
+                    temp = a + b;
+                    a = b;
+                    b = temp;
                 }
             }
 
-            media = soma / qtd;
-
-            diasAcima = valores.Count(v => v > media);
-
-            Console.WriteLine("Dia Menor faturamento: " + menor);
-            Console.WriteLine(" Dia Maior faturamento: " + maior);
-            Console.WriteLine("Média mensal: " + media);
-            Console.WriteLine("Dias com faturamento acima da média: " + diasAcima);
+            return valido;
         }
     }
 }
