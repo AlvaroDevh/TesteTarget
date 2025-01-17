d7f22ed7172c3f3a656feea70ce3ce9f2721fc80
project <joaoalvaro700@gmail.com>
2025-01-16 22:32:17 -0300

    Exer05

---
+++ Exer05/Exer05/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exer05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite a palavra");
+            string palavra = Console.ReadLine();
+            string palavraInvertida = "";
+
+            for (int i = palavra.Length - 1; i >= 0; i--)
+            {
+                palavraInvertida += palavra[i];
+            }
+
+            Console.WriteLine("Palavra invertida: " + palavraInvertida);
+
+        }
+    }
+}
